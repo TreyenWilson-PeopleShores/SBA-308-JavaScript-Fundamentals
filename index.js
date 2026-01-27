16c8883d3d75b9077a5712827dc7eaa5883b44eb
TreyenWilson-PeopleShores <treyen.wilson@peopleshores.com>
2026-01-27 13:26:47 -0500

    Made learner array

--- index.js
+++ index.js
@@ -1,91 +1,108 @@
+// The provided course information.
 const CourseInfo = {
-    id: 451,
-    name: "Introduction to JavaScript"
+  id: 451,
+  name: "Introduction to JavaScript"
 };
 
 // The provided assignment group.
 const AssignmentGroup = {
-    id: 12345,
-    name: "Fundamentals of JavaScript",
-    course_id: 451,
-    group_weight: 25,
-    assignments: [
-        {
-            id: 1,
-            name: "Declare a Variable",
-            due_at: "2023-01-25",
-            points_possible: 50
-        },
-        {
-            id: 2,
-            name: "Write a Function",
-            due_at: "2023-02-27",
-            points_possible: 150
-        },
-        {
-            id: 3,
-            name: "Code the World",
-            due_at: "3156-11-15",
-            points_possible: 500
-        }
-    ]
+  id: 12345,
+  name: "Fundamentals of JavaScript",
+  course_id: 451,
+  group_weight: 25,
+  assignments: [
+    {
+      id: 1,
+      name: "Declare a Variable",
+      due_at: "2023-01-25",
+      points_possible: 50
+    },
+    {
+      id: 2,
+      name: "Write a Function",
+      due_at: "2023-02-27",
+      points_possible: 150
+    },
+    {
+      id: 3,
+      name: "Code the World",
+      due_at: "3156-11-15",
+      points_possible: 500
+    }
+  ]
 };
 
 // The provided learner submission data.
 const LearnerSubmissions = [
-    {
-        learner_id: 125,
-        assignment_id: 1,
-        submission: {
-            submitted_at: "2023-01-25",
-            score: 47
-        }
-    },
-    {
-        learner_id: 125,
-        assignment_id: 2,
-        submission: {
-            submitted_at: "2023-02-12",
-            score: 150
-        }
-    },
-    {
-        learner_id: 125,
-        assignment_id: 3,
-        submission: {
-            submitted_at: "2023-01-25",
-            score: 400
-        }
-    },
-    {
-        learner_id: 132,
-        assignment_id: 1,
-        submission: {
-            submitted_at: "2023-01-24",
-            score: 39
-        }
-    },
-    {
-        learner_id: 132,
-        assignment_id: 2,
-        submission: {
-            submitted_at: "2023-03-07",
-            score: 140
-        }
+  {
+    learner_id: 125,
+    assignment_id: 1,
+    submission: {
+      submitted_at: "2023-01-25",
+      score: 47
     }
+  },
+  {
+    learner_id: 125,
+    assignment_id: 2,
+    submission: {
+      submitted_at: "2023-02-12",
+      score: 150
+    }
+  },
+  {
+    learner_id: 125,
+    assignment_id: 3,
+    submission: {
+      submitted_at: "2023-01-25",
+      score: 400
+    }
+  },
+  {
+    learner_id: 132,
+    assignment_id: 1,
+    submission: {
+      submitted_at: "2023-01-24",
+      score: 39
+    }
+  },
+  {
+    learner_id: 132,
+    assignment_id: 2,
+    submission: {
+      submitted_at: "2023-03-07",
+      score: 140
+    }
+  }
 ];
 
 
-function AnalyzeData(AssignmentGroup, LearnerSubmissions, CourseInfo){
 
-        for(let x = 0; x<CourseInfo.length; x+=1){ //This is the loop to go through all the CourseInfo data
-            for(let y =0; y<AssignmentGroup.assignments.length; y+=1){ //This is going through the assignments in assignmentgroup
-                if(CourseInfo[x].assignment_id===AssignmentGroup.assignments[y].id)       // Checks to see if assignment exists
-                {
-                    console.log("It equals");
-                }
-            }
-        }
-    
+function getLearnerData(course, ag, submissions) {
+    let learners = [{learner_id: 0, averageScore: 1, a1:.5, a2: .4, a3:.3,},{learner_id: 0, averageScore: 1, a1:.5, a2: .4, a3:.3,}]; // This array is to store learner information
+    if(course.id!==ag.course_id){ // Checks to see if the course id is valid, if not, sends an error.
+        throw new Error("Incorrect course ID.");
+    }
+
+
+    const result = [ // use this for format
+    {
+        id: submissions[x].learner_id,
+        avg: 0.985, // (47 + 150) / (50 + 150)
+        1: 0.94, // 47 / 50
+        2: 1.0 // 150 / 150
+        },
+        {
+        id: 132,
+        avg: 0.82, // (39 + 125) / (50 + 150)
+        1: 0.78, // 39 / 50
+        2: 0.833 // late: (140 - 15) / 150
+    }
+    ];
+
+    return result;
 }
-AnalyzeData(AssignmentGroup, LearnerSubmissions, CourseInfo)
+
+const result = getLearnerData(CourseInfo, AssignmentGroup, LearnerSubmissions);
+
+console.log(result);
